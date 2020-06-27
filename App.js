701f23473f5e61746ce3fd4d59005b52f925a2ac
kzsteph <kexin.s.zhang@gmail.com>
2020-06-27 15:28:44 -0400

    Build Page Navigation

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import * as React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button } from 'react-native-elements';
+import Form from './components/Form';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style = {styles.button}>
+    <Button
+      buttonStyle={{backgroundColor: '#008b8b',width:120}}
+      title="Create New Name Card"
+      onPress={() => {
+        navigation.navigate('Name Card Form');
+      }}
+    />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Name Card Form" component={Form}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,5 +39,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    flexDirection: 'row',
   },
 });
---
+++ components/Form.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button } from 'react-native-elements';
+
+
+export default function Form({ route, navigation }) {
+  return (
+    <Text>nothing here</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
