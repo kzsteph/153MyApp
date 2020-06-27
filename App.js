ebb83c63c8c1337878c007dcb6793b7f6a13b4f4
kzsteph <kexin.s.zhang@gmail.com>
2020-06-27 18:39:35 -0400

    Add Form, and several other functional components to collect data and show them on main page. Finished minimal app.

--- App.js
+++ App.js
@@ -1,23 +1,95 @@
 
-import * as React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList,ImageBackground} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useAsyncStorage } from '@react-native-community/async-storage';
 import { Button } from 'react-native-elements';
 import Form from './components/Form';
+import greyback from './assets/greyback.jpg';
 
-function HomeScreen({navigation}) {
+function HomeScreen({route,navigation}) {
+
+  const sampleItem =
+    {name:'kexin', phone:'7815471811', email:'zxksteph@gmail.com', address:'415 South Street',
+    school:'Brandeis', major:'Econ, Applied Math, CS', year:'2021'}
+
+  const [cards, setCards] = useState([sampleItem])
+  const { getItem, setItem } = useAsyncStorage('CardBox')
+
+  const readItemFromStorage = async () => {
+    try {
+      let item = await getItem();
+      let newValue = JSON.parse(item);
+      newValue = newValue || [sampleItem]
+      setCards(newValue);
+    }
+    catch(e) {
+      setCards([sampleItem])
+    }
+  };
+
+  const writeItemToStorage = async newValue => {
+    await setItem(JSON.stringify(newValue));
+    setCards(newValue);
+  };
+
+  useEffect(() => {
+    readItemFromStorage();
+  }, []);
+
+  const addCard = (card) => {
+    card.key = Math.random().toString();
+    writeItemToStorage([card,...cards]);
+  }
+
+  const deleteItem = (key) => {
+    const newCards = cards.filter(x => x.key!=key);
+    writeItemToStorage(newCards);
+  }
+
   return (
+    <ImageBackground source={greyback} style={styles.backgroundImage}>
     <View style = {styles.button}>
     <Button
-      buttonStyle={{backgroundColor: '#008b8b',width:120}}
+      buttonStyle={{backgroundColor: '#008b8b',width:240}}
       title="Create New Name Card"
       onPress={() => {
-        navigation.navigate('Name Card Form');
+        navigation.navigate('Name Card Form',{addCard});
       }}
     />
+    <FlatList style = {{marginTop:15}}
+      data={cards}
+      keyExtractor={(item, index) => 'key'+index}
+      renderItem={({ item }) => <CardFormat card = {item} deleteItem = {deleteItem}/>} //create a Card.js to regulate the format
+    />
     </View>
+    </ImageBackground>
   );
+}
+
+function CardFormat({card,deleteItem}) {
+  return (
+      <Card>
+
+      <Text style ={styles.cardtext}> Name:{card.name} Year:{card.year}</Text>
+      <Button
+        title = 'delete'
+        buttonStyle={{backgroundColor: 'maroon', height:20, width:200}}
+        onPress={() => {deleteItem(card.key)}}
+      />
+      </Card>
+  );
+}
+
+function Card(props) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardContent}>
+        { props.children }
+      </View>
+    </View>
+  )
 }
 
 const Stack = createStackNavigator();
@@ -41,6 +113,32 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   button: {
-    flexDirection: 'row',
+    marginTop:15,
+    alignItems: 'center',
+  },
+  cardtext: {
+    marginTop:7,
+    marginBottom:7,
+    marginLeft:7,
+    marginRight:7,
+  },
+  card:{
+    borderRadius: 6,
+    elevation: 3,
+    backgroundColor: '#fff',
+    shadowOffset: {width: 1, height: 1},
+    shadowColor: '#333',
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    marginHorizontal: 4,
+    marginVertical: 6,
+  },
+  cardContent:{
+    marginHorizontal: 18,
+    marginVertical: 12,
+  },
+  backgroundImage: {
+    height:'100%',
+    width:'100%',
   },
 });
--- components/Form.js
+++ components/Form.js
@@ -1,13 +1,84 @@
 import * as React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View,TextInput,ImageBackground} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Button } from 'react-native-elements';
+import { Formik } from 'formik';
+import greyback from '../assets/greyback.jpg';
 
+export default function Form({ route,navigation }) {
 
-export default function Form({ route, navigation }) {
+const {addCard} = route.params;
+
   return (
-    <Text>nothing here</Text>
+    <ImageBackground source={greyback} style={styles.backgroundImage}>
+    <View style={styles.container}>
+    <Formik
+      initialValues={{name:'', phone:'', email:'', address:'', school:'', major:'', year:''}}
+      onSubmit={(values, actions)=>{
+        actions.resetForm();
+        addCard(values);
+      }}
+    >
+      {(props)=>(
+        <View >
+          <TextInput
+            style={styles.input}
+            placeholder='your name'
+            placeholderTextColor = '#696969'
+            onChangeText={props.handleChange('name')}
+            value={props.values.name}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='your phone number'
+            placeholderTextColor = '#696969'
+            onChangeText={props.handleChange('phone')}
+            value={props.values.phone}
+            keyboardType='numeric'
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='your email address'
+            placeholderTextColor = '#696969'
+            onChangeText={props.handleChange('email')}
+            value={props.values.email}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='house address'
+            placeholderTextColor = '#696969'
+            onChangeText={props.handleChange('address')}
+            value={props.values.address}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='your school'
+            placeholderTextColor = '#696969'
+            onChangeText={props.handleChange('school')}
+            value={props.values.school}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='your major(s)'
+            placeholderTextColor = '#696969'
+            onChangeText={props.handleChange('major')}
+            value={props.values.major}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='school year'
+            placeholderTextColor = '#696969'
+            onChangeText={props.handleChange('year')}
+            value={props.values.year}
+            keyboardType='numeric'
+          />
+          <Button title='submit' color='maroon' onPress={props.handleSubmit} />
+        </View>
+      )}
+      </Formik>
+    </View>
+    </ImageBackground>
   );
 }
 
@@ -15,5 +86,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 6,
+    alignItems: 'center',
+    color:'black',
+  },
+  backgroundImage: {
+    height:'100%',
+    width:'100%',
   },
 });
